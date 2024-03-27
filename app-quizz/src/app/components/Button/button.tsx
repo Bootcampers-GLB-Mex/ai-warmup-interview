97d60feefe55ed832d845bb93b0d51dd51cd64d1
Cinthya Gutierrez <cinthya.gutierrez@globant.com>
2024-03-27 17:18:09 -0600

    Add new admin page development

--- app-quizz/src/app/components/Button/button.tsx
+++ app-quizz/src/app/components/Button/button.tsx
@@ -1,13 +1,19 @@
 type ButtonProps = {
   title: string;
+  style?: React.CSSProperties;
   handlerEvent: () => void;
 };
 
 const Button = (props: ButtonProps) => {
+  const {style} = props;
   const btnStyles =
-    "bg-teal-900 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded";
+    "bg-teal-900 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded ";
   return (
-    <button className={btnStyles} onClick={props.handlerEvent}>
+    <button
+      className={btnStyles}
+      style={{...style}}
+      onClick={props.handlerEvent}
+    >
       {props.title}
     </button>
   );
