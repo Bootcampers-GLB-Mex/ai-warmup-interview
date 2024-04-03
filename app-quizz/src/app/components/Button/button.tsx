ca51c3e84a85c87e54a86d22f812d2e59ed5dfd2
Cinthya Gutierrez <cinthya.gutierrez@globant.com>
2024-04-03 14:34:06 -0600

    Resolve Pr comments

--- app-quizz/src/app/components/Button/button.tsx
+++ app-quizz/src/app/components/Button/button.tsx
@@ -9,17 +9,12 @@ type ButtonProps = {
 
 const Button = (props: ButtonProps) => {
   const {style} = props;
+
   const btnStyles =
     "bg-teal-900 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded ";
 
-  const classes = classNames(btnStyles, style);
-
   return (
-    <button
-      className={btnStyles}
-      onClick={props.handlerEvent}
-      style={{...style}}
-    >
+    <button className={btnStyles} onClick={props.handlerEvent} style={style}>
       {props.title}
     </button>
   );
