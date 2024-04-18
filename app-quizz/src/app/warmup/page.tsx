b85df8c82fa86510678cfad1797c31989dbcc768
Faviola Narvaez <marcela.narvaez@globant.com>
2024-04-18 14:33:17 -0600

    Working adding new action

--- app-quizz/src/app/warmup/page.tsx
+++ app-quizz/src/app/warmup/page.tsx
@@ -2,13 +2,21 @@
 
 import { Dialog } from '@headlessui/react';
 import { useState } from 'react';
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik';
 
 import Button from '@/components/Button/button';
 import InputField from '@/components/InputField/InputField';
 
+import { setAccessCode } from '../authentication/actions';
+
 export default function Warmup() {
   let [isOpen, setIsOpen] = useState(true);
+
+  const handleGetAccessCode = async (values: { accessCode: string }) => { 
+    const result = await setAccessCode(values.accessCode);
+    console.log(result, 'Error 404');
+    setIsOpen(false);
+  };
 
   return (
     <div className="md:container md:mx-auto min-h-full ml-4 mr-4 pt-32 pb-20">
@@ -35,14 +43,17 @@ export default function Warmup() {
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-10">
             <Dialog.Title>Get your Warmup</Dialog.Title>
-            <Formik initialValues={{ accessCode: '' }} onSubmit={() => {}}>
-              <InputField
-                name="accessCode"
-                label="Access Code"
-                placeholder="Enter your access code"
-              />
+            <Formik initialValues={{ accessCode: '' }} onSubmit={handleGetAccessCode}>
+            <Form className="mt-8 space-y-6">
+                <InputField
+                  id="accessCode"
+                  name="accessCode"
+                  label="Access Code"
+                  placeholder="Enter your access code"
+                />
+                <Button type='submit'>Submit</Button>
+              </Form>
             </Formik>
-            <Button type='submit'>Submit</Button>
           </Dialog.Panel>
         </div>
       </Dialog>
